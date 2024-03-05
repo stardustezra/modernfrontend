789d781906133a138ebf8c7a978ad3a4d2f70fb0
Ezra Andersen <stardustezra@gmail.com>
2024-03-05 07:59:11 +0100

    main

--- testProject/src/main.js
+++ testProject/src/main.js
@@ -1,14 +1,30 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(createPinia())
-app.use(router)
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoSg7QcdUbKPx2qU0Mz-Oq_3_zHOoLsUU",
+  authDomain: "fir-aef23.firebaseapp.com",
+  projectId: "fir-aef23",
+  storageBucket: "fir-aef23.appspot.com",
+  messagingSenderId: "260906830996",
+  appId: "1:260906830996:web:c2e8190d55141f43c5f881",
+};
 
-app.mount('#app')
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
